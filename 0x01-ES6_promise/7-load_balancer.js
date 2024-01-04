a81163d77050ed6b5f3e92804d01f93bfa72fc06
Adane <adanemoges6@gmail.com>
2024-01-04 03:41:50 +0300

    task 7

--- 0x01-ES6_promise/7-load_balancer.js
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -1,8 +1,4 @@
-import { promises } from 'dns';
-import { signUpUser } from './4-user-promise';
-import { uploadPhoto } from './5-photo-reject';
-export default function loadBalancer (chinaDownload, USDownload) {
-  return promises((resolve) => {
-    resolve(signUpUser(chinaDownload, USDownload));
-  });
+export default function loadBalancer(chinaDownload, USDownload) {
+  return Promise
+    .race([chinaDownload, USDownload]);
 }
