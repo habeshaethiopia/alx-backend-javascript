26a00a2215ffb0334f96c8ce21a333fe70f1508b
Adane <adanemoges6@gmail.com>
2024-01-06 02:07:52 +0300

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+  }
+
+  set sqft(newSqft) {
+    if (typeof sqft !== 'number') {
+      throw TypeError('amount must be a number');
+    }
+    this._sqft = newSqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
+export default Building;
