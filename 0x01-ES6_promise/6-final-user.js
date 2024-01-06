15f5bc2a164fa42694439f095d5f98681eecd95e
Adane <adanemoges6@gmail.com>
2024-01-06 03:58:29 +0300

    task 6 updated

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,10 +3,15 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
   return Promise.allSettled([uploadPhoto(filename), signUpUser(firstName, lastName)])
-    .then((value) => {
-      value.map((val) => ({
-        status: val.status,
-        value: val.status === 'fulfilled' ? val.value : String(val.reason),
-      }));
+    .then((values) => {
+      const result = [];
+      values.forEach((element) => {
+        if (element.status === 'fulfilled') {
+          result.push({ status: element.status, value: element.value });
+        } else {
+          result.push({ status: element.status, value: `${element.reason}` });
+        }
+      });
+      return result;
     });
 }
