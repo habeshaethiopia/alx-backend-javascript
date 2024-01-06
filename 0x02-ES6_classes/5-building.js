c685ae6c6927c24aa051f8804315e07042a89806
Adane <adanemoges6@gmail.com>
2024-01-06 12:09:52 +0300

    task 5 after second deadline eslint fails

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,17 +2,17 @@ class Building {
   constructor(sqft) {
     this._sqft = sqft;
     if (this.constructor !== Building) {
-      if (typeof this.evacuationWarningMessage !== "function") {
+      if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
-          "Class extending Building must override evacuationWarningMessage"
+          'Class extending Building must override evacuationWarningMessage',
         );
       }
     }
   }
 
   set sqft(newSqft) {
-    if (typeof sqft !== "number") {
-      throw TypeError("amount must be a number");
+    if (typeof sqft !== 'number') {
+      throw TypeError('amount must be a number');
     }
     this._sqft = newSqft;
   }
