f2d1723350af1b861f3a7de0d383fec732735bc7
Adane <adanemoges6@gmail.com>
2024-01-09 23:18:31 +0300

    task 0

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,10 @@
 function getListStudents() {
   return [
-    { id: 1, firstName: "Guillaume", location: "San Francisco" },
+    {
+      id: 1,
+      firstName: "Guillaume",
+      location: "San Francisco",
+    },
     {
       id: 2,
       firstName: "James",
