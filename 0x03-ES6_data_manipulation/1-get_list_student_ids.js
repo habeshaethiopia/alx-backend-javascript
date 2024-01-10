fc717d124d7fb8e2f218613e4293382c2d426411
Adane <adanemoges6@gmail.com>
2024-01-10 05:59:37 +0300

    all

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,8 @@
 export default function getListStudentIds(arr) {
-  var ans = [];
-  for (var i = 0; i < arr.length; i++) {
-    ans.push(arr[i]["id"]);
+    let newArr = [];
+    if (arr instanceof Array) {
+      newArr = arr.map((item) => item.id);
+    }
+  
+    return newArr;
   }
-  return ans;
-}
