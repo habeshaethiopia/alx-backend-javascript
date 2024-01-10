af01d5ba9d460f08b4f5b9ab7ae00989ca44cb80
Adane <adanemoges6@gmail.com>
2024-01-10 07:04:47 +0300

    tasak4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,4 +1,15 @@
-export default function updateStudentGradeByCity(arr, city, newGrades)
-{
-    
+export default function updateStudentGradeByCity(arr, city, newGrades) {
+  const ext = arr.filter((a) => a.location === city);
+  const nw = ext.map((student) => {
+    for (const stu of newGrades) {
+      if (student.id === stu.studentId) {
+        student.grade = stu.grade; // eslint-disable-line no-param-reassign
+      }
+    }
+    if (student.grade === undefined) {
+      student.grade = 'N/A'; // eslint-disable-line no-param-reassign
+    }
+    return student;
+  });
+  return nw;
 }
