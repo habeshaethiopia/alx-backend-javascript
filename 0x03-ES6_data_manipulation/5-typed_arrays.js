d70bede244f359d7468909bbc9fc4654d4100321
Adane <adanemoges6@gmail.com>
2024-01-10 08:44:24 +0300

    task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,8 @@
-export default function createInt8TypedArray(length, position, value) {
+function createInt8TypedArray(length, position, value) {
   if (position >= length) {
     throw new Error('Position outside range');
   }
   const buff = new DataView(new ArrayBuffer(length), 0, length);
   buff.setInt8(position, value);
+  return buff;
 }
