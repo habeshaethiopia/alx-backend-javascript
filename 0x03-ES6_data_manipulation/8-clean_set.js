87a7f6f2757f0da7948e517f6b310f580a3e345b
Adane <adanemoges6@gmail.com>
2024-01-10 13:12:20 +0300

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(s, str) {
   const clean = [];
   for (const val of s) {
-    if (val.startsWith(str)) {
+    if (val.startsWith(str) && typeof val === "string") {
       clean.push(val.substr(str.length));
     }
   }
