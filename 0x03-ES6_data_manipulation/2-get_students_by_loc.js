78376b82b49eb8d157081f81cf2b60edade4e215
Adane <adanemoges6@gmail.com>
2024-01-10 06:00:17 +0300

    all

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,10 +1,3 @@
-export default function getStudentsByLocation(arr, loc)
-{
-    var ans = [];
-    for (var i = 0; i < arr.length; i++) {
-      if arr[i]['location']==loc:
-          ans.push(arr[i]);
-
-    }
-    return ans
+export default function getStudentsByLocation(students, city) {
+  return students.filter((student) => student.location === city);
 }
