482e4f50281033da85823333d26745b53aa56d12
Adane <adanemoges6@gmail.com>
2024-01-13 05:00:38 +0300

    abcd

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,16 +1,15 @@
 export default function cleanSet(s, str) {
   const clean = [];
-  if (
-    !s
-    && !str
-    && !(s instanceof Set)
-    && typeof str !== 'string'
-  ) {
+  if (!s && !str && !(s instanceof Set) && typeof str !== 'string') {
     return '';
   }
-  for (const val of s) {
-    if (val.startsWith(str) && typeof val === 'string') {
-      clean.push(val.substr(str.length));
+  for (const value of s.values()) {
+    if (typeof value === 'string' && value.startsWith(str)) {
+      const valueSubStr = value.substring(str.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        clean.push(valueSubStr);
+      }
     }
   }
   return clean.join('-');
