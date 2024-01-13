47cde4f4b6a9bc7cf45d9178c96d30963e0c15d3
Adane <adanemoges6@gmail.com>
2024-01-13 05:57:28 +0300

    task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,7 +1,3 @@
 export default function getStudentIdsSum(arr) {
-  let ans = 0;
-  for (let i = 0; i < arr.length; i += 1) {
-    ans += arr[i].id;
-  }
-  return ans;
+  return arr.reduce((agg, student) => agg + student.id, 0);
 }
