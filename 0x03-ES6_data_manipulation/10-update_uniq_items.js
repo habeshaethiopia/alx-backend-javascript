57e7c42a9815a25c77d2c545111d26ab3fdeb044
Adane <adanemoges6@gmail.com>
2024-01-13 05:32:00 +0300

    task 10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(items) {
+  if (!(items instanceof Map)) {
+    throw new Error('Cannot process');
+  }
   items.forEach((key, value) => {
     if (key === 1) {
       items.set(value, 100);
